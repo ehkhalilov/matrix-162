88b32cc718b3eef3f7aeffa64443223a79524b44
19Alicanov05 <alicanovimran@gmail.com>
2024-09-23 20:24:12 +0400

    salam

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    static void toplama (int a, int b) {
+        System.out.println(a+b);
+    }
+    static void cixma (int a, int b) {
+        System.out.println(a-b);
+    }
+    static void vurma (int a, int b) {
+        System.out.println(a*b);
+    }
+    static void bolme (int a, int b) {
+        System.out.println(a/b);
+    }
+}
